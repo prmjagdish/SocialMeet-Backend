70614412138e3033c1253ad609d503daaff0e9f9
jagdish <jaagddissh@gmail.com>
2025-08-15 14:58:24 +0530

    Credentials allow

--- src/main/java/com/jagdish/SocailSphere/security/SecurityConfig.java
+++ src/main/java/com/jagdish/SocailSphere/security/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // React app
+        config.setAllowedOrigins(List.of("https://socialsphereweb.vercel.app")); // React app
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true); // if you use cookies/sessions
